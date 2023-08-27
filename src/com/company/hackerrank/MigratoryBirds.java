ebec1bc25c29353c8a2c5eaa7018ceac6b103add
Bilal <m.b1lal@hotmail.com>
2023-08-27 20:20:56 +0400

    Solved MaximumPermieterTriangle

--- src/com/company/hackerrank/MigratoryBirds.java
+++ src/com/company/hackerrank/MigratoryBirds.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 
 /**
  * Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
