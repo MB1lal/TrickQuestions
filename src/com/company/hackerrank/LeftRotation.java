2eb1299bc0cb8796c243bfcfacc8055a3ac651de
Muhammad Bilal Awan <54260933+MB1lal@users.noreply.github.com>
2023-09-01 21:27:24 +0400

    Create LeftRotation.java

---
+++ src/com/company/hackerrank/LeftRotation.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+/**
+
+A left rotation operation on an array of size  shifts each of the array's elements  unit to the left. Given an integer, , rotate the array that many steps left and return the result.
+
+Example
+
+
+After  rotations, .
+
+Function Description
+
+Complete the rotateLeft function in the editor below.
+
+rotateLeft has the following parameters:
+
+int d: the amount to rotate by
+int arr[n]: the array to rotate
+Returns
+
+int[n]: the rotated array
+Input Format
+
+The first line contains two space-separated integers that denote , the number of integers, and , the number of left rotations to perform.
+The second line contains  space-separated integers that describe .
+
+*/
+
+class Result {
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+    // Write your code here
+        List<Integer> result = arr.subList(d, arr.size());
+        result.addAll(arr.subList(0, d));
+        return result;
+    }
+
+}
